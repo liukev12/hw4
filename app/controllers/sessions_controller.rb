28fa1147fa20d19d45db635acea9e2357fa77efc
Kevin Liu <kevinliu5890@gmail.com>
2024-06-04 17:08:36 +0000

    commit 6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     @user = User.find_by({"email" => params["email"]})
     if @user != nil
-      if BCrypt::Password.new(@user["password"]) == params["password"]
+      if @user && @user.authenticate(params[:password])
       session["user_id"] = @user["id"]
       flash["notice"] = "Hello!"
       redirect_to "/places"    
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     if User.find_by({"email" => params["email"]})
-      flash["notice"] = "you have already be a user"
+      flash["notice"] = "you are already a user"
       redirect_to "/users/new"
     else  
       @user["username"] = params["username"]
       @user["email"] = params["email"]
       @user["password"] = BCrypt::Password.create(params["password"])
-      @user.save
-      redirect_to "/"
+      if @user.save
+        session[:user_id] = @user.id  # Log in the user after sign up
+        redirect_to "/"
+      else
+        render "new"  # Render the form again if save fails
+      end
     end
   end
+end
+private
+
+def user_params
+  params.require(:user).permit(:username, :email, :password, :password_confirmation)
+end
 end
